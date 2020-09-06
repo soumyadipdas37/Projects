4567b0cc3b15c0f756e25ad1c19a218304f460cf
rsgbengi <62507917+rsgbengi@users.noreply.github.com>
2020-09-06 18:55:57 +0200

    Pdf pruebas

--- WebScraper/scraper.py
+++ WebScraper/scraper.py
@@ -1,23 +1,37 @@
 from bs4 import BeautifulSoup 
 import requests 
-import csv 
-source= requests.get('https://blog.erratasec.com')
+from fpdf import FPDF
 
-soup=BeautifulSoup(source.text,'lxml')
-csv_file=open('csm_scrape.csv','w')
 
-csv_writer=csv.writer(csv_file)
-csv_writer.writerow(['Title','Summary','FullArticleLink'])
-for article in soup.find_all('div',class_='date-outer'):
-    try:
-        #title=article.find('div',class_=('date-outer')).h3.a.text
-        title=article.h3.a.text
-        summary=article.find('div',class_=('post-body entry-content')).text
-        link=article.find('div',class_='jump-link').a['href']
-        print(title+summary+link)
-        print("ESTO ES OTRO ARTICULO")
-    except Exception as e:
-        pass
-    csv_writer.writerow([title,summary,link])
+def errataSec():
+    pdf=FPDF()
+    #pdf.cell(100,100,txt="Esto es una",ln=2,align="C")
+    
+    source= requests.get('https://blog.erratasec.com')
+    soup=BeautifulSoup(source.text,'lxml')
+    for article in soup.find_all('div',class_='date-outer'):
+        try:
+            
+            #title=article.find('div',class_=('date-outer')).h3.a.text
+            title=article.h3.a.text
+            summary=article.find('div',class_=('post-body entry-content')).text
+            link=article.find('div',class_='jump-link').a['href']
+            #print(title+summary+link)
+            #print("ESTO ES OTRO ARTICULO")
+            print(title.encode('latin-1'))
+            pdf.add_page()
+            pdf.set_font("Arial",'UI',size=30)
+            pdf.cell(100,40,txt=title.decode('latin-1'),align="C")
+            pdf.ln()
+            pdf.set_font("Arial",size=12)
+            pdf.multi_cell(0,5,txt=summary.decode('latin-1'))
+            #pdf.cell(100,100,txt="Esto es una",ln=2,align="C")
+            pdf.ln()
+        
+        except Exception as e:
+            pass
+    pdf.output("prueba.pdf")
 
-csv_file.close()
+errataSec()
+ 
+
